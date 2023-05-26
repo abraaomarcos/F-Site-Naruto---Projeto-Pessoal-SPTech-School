242587d7633802ba13433dfa3187284bcc4bf509
abraaomarcos <abraao.m3@gmail.com>
2023-05-26 15:43:18 -0300

    adicionei tabelas ao BD. Tive problemas com os relacionamentos e consertarei ASAP

---
+++ Banco de Dados/Script BD Naruto.sql
@@ -0,0 +1,21 @@
+-- CREATE DATABASE naruto;
+-- USE naruto;
+
+CREATE TABLE personagem(
+idPersonagem INT,
+nomePersonagem VARCHAR(45),
+descricao VARCHAR(100),
+energia DOUBLE,
+fase VARCHAR(45),
+constraint chkFase CHECK(fase in('genin', 'chunin', 'jonin')),
+nivel VARCHAR(45),
+constraint chkNivel CHECK(nivel in('elite', 'kage'))
+);
+
+ CREATE TABLE usuario(
+ idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+ nomeUsuario VARCHAR(45),
+ email VARCHAR(45),
+ senha VARCHAR(45),
+ fkPersonagem INT
+ );
