66c097e6a3a594671bae9fcc1b0983d1c50e735d
abraaomarcos <abraao.m3@gmail.com>
2023-05-31 01:06:18 -0300

    adicionei tela de quiz. Vou melhorar a estilização ainda

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,67 @@
-function conferir(){
-    var comportamento = document.getElementsByName('comporta')
-    var style = document.getElementsByName('estilo')
-    var persona = document.getEunctlementsByName('champ')
-    var res = document.querySelector('div#res')
-    var img = document.createElement('img')
-    img.setAttribute('id' , 'foto')
-
-  if (comportamento[0].checked && style[0].checked && persona[1 || 3]){
-     
-     img.setAttribute('src' , 'Top_icon.png')
-     res.innerHTML = 'testes' 
-     res.appendChild(img)
-  }
+function submitQuiz() {
+   var form = document.getElementById("quiz-form");
+   var answers = form.elements;
+   var scores = {
+     garra: 0,
+     esforco: 0,
+     foco: 0,
+     inteligencia: 0,
+     raciocinio: 0,
+     responsabilidade: 0,
+     moderacao: 0,
+     generosidade: 0,
+     autoestima: 0,
+     apaixonada: 0
+   };
+ 
+   // Verifica se todas as perguntas foram respondidas
+   var allQuestionsAnswered = true;
+   for (var i = 0; i < answers.length; i++) {
+     if (answers[i].value === "") {
+       allQuestionsAnswered = false;
+       break;
+     }
+   }
+ 
+   // if (i < answers.length) {
+   //   var errorMessage = document.getElementById("error-message");
+   //   errorMessage.textContent = "Por favor, responda todas as perguntas.";
+   //   return;
+   // }
+ 
+   // Calcula a pontuação com base nas respostas selecionadas
+   for (var i = 0; i < answers.length; i++) {
+     var selectedValue = answers[i].value;
+     scores[selectedValue]++;
+   }
+ 
+   // Determina o resultado com base na pontuação
+   var result = document.getElementById("result");
+   var highestScore = 0;
+   var character = "";
+ 
+   // Objeto com os nomes dos personagens
+   var characterNames = {
+     garra: "Naruto Uzumaki",
+     esforco: "Rock Lee",
+     foco: "Sasuke Uchiha",
+     inteligencia: "Sakura Haruno",
+     raciocinio: "Kakashi Hatake",
+     responsabilidade: "Tsunade Senju",
+     moderacao: "Jiraiya",
+     generosidade: "Gaara",
+     autoestima: "Neji Hyuga",
+     apaixonada: "Hinata Hyuga"
+   };
+ 
+   for (var score in scores) {
+     if (scores[score] > highestScore) {
+       highestScore = scores[score];
+       character = score;
+     }
+   }
+ 
+   // Exibe o resultado na tela
+   result.innerHTML = "Personagem associado: " + characterNames[character];
+ }
+ 
